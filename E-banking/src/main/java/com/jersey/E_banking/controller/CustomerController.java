e428d03e02fe7905d71ad2b7d2b5611d88ade74f
RESTY NASIMBWA <54865360+restynan@users.noreply.github.com>
2020-03-15 21:39:24 -0500

    @PathParam / PathVariable

--- E-banking/src/main/java/com/jersey/E_banking/controller/CustomerController.java
+++ E-banking/src/main/java/com/jersey/E_banking/controller/CustomerController.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -23,7 +24,7 @@ public class CustomerController {
 	}
 	
 	@GET
-	@Produces (MediaType.APPLICATION_JSON)
+	@Produces ({MediaType.APPLICATION_JSON,MediaType.APPLICATION_JSON})
 	public List<Customer> getCustomers()
 	{ 
 		return service.getAllCustomers();
@@ -31,14 +32,14 @@ public class CustomerController {
 	
 				}
 	@GET
-	@Path("customer")
-	@Produces (MediaType.APPLICATION_JSON)
-	public Customer getCustomer(int id)
+	@Path("customer/{id}")
+	@Produces ({MediaType.APPLICATION_JSON,MediaType.APPLICATION_JSON})
+	public Customer getCustomer(@PathParam("id") int id)
 	{ 
-		return service.getCustomer(id);
-	
+		return service.getCustomer(1);
 	
 	}
+	
 	@POST
 	@Path("customer")
 	@Consumes (MediaType.APPLICATION_JSON)
